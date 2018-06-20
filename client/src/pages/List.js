957744d302c4c595b33b17666d4f57e543dfc63d
Hannah Patellis <hannah@dhcp-10-103-28-237.wireless.northwestern.private>
2018-06-20 11:28:17 -0500

    Starts List component

---
+++ client/src/pages/List.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class List extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <h1>The list page!</h1>
+        )
+    }
+}
+
+export default List
